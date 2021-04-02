7855a6a6f0574717ad8ce42cab973a390a2b0556
sim <sim@terra.money>
2021-04-02 15:00:36 +0900

    Show delisted tokens on the modal

--- src/layouts/DelistAlert.tsx
+++ src/layouts/DelistAlert.tsx
@@ -1,24 +1,44 @@
+import { uniq } from "ramda"
 import useMy from "../pages/My/useMy"
 import DelistModal from "./DelistModal"
 
 const DelistAlert = () => {
   const my = useMy()
-  console.log(my)
   const { holdings, mint, pool, stake, orders } = my
 
-  const hasDelist = [
-    holdings,
-    mint,
-    pool,
-    stake,
-    orders,
-  ].some(({ dataSource }) =>
-    dataSource.some(
-      ({ status }: { status: ListedItemStatus }) => status === "DELISTED"
-    )
-  )
+  const delistedHoldings = holdings.dataSource.filter(filter).map(getToken)
 
-  return hasDelist ? <DelistModal /> : null
+  const delistedMintTokens = mint.dataSource
+    .filter(filter)
+    .reduce<string[]>(
+      (acc, { collateral, asset }) =>
+        acc
+          .concat(collateral.delisted ? collateral.token : [])
+          .concat(asset.delisted ? asset.token : []),
+      []
+    )
+
+  const delistedPoolTokens = pool.dataSource.filter(filter).map(getToken)
+  const delistedStakedTokens = stake.dataSource.filter(filter).map(getToken)
+  const delistedOrderTokens = orders.dataSource
+    .filter(filter)
+    .map(({ asset }) => asset.token)
+
+  const delistedTokens = uniq([
+    ...delistedHoldings,
+    ...delistedMintTokens,
+    ...delistedPoolTokens,
+    ...delistedStakedTokens,
+    ...delistedOrderTokens,
+  ])
+
+  return delistedTokens.length ? <DelistModal tokens={delistedTokens} /> : null
 }
 
 export default DelistAlert
+
+/* helpers */
+const filter = <T extends { status: ListedItemStatus }>({ status }: T) =>
+  status === "DELISTED"
+
+const getToken = <T extends { token: string }>({ token }: T) => token
