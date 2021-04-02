d72d71c0d11218269613ea010d26eec7523f267c
sim <sim@terra.money>
2021-04-02 16:04:41 +0900

    design: Migrate delisted asset

--- src/layouts/DelistModal.tsx
+++ src/layouts/DelistModal.tsx
@@ -3,6 +3,8 @@ import { format } from "../libs/parse"
 import { useContract, useContractsAddress } from "../hooks"
 import { PriceKey } from "../hooks/contractKeys"
 import Card from "../components/Card"
+import Button from "../components/Button"
+import ExtLink from "../components/ExtLink"
 import Modal, { useModal } from "../containers/Modal"
 import styles from "./DelistModal.module.scss"
 
@@ -11,49 +13,75 @@ const DelistModal = ({ tokens }: { tokens: string[] }) => {
   const { getSymbol } = useContractsAddress()
   const { find } = useContract()
   const modal = useModal(true)
+  const symbols = tokens.map(getSymbol).join(", ")
 
   return (
     <Modal {...modal}>
-      <Card>
-        <p>
-          The following asset will be delisted due to a stock split / merge.
-        </p>
+      <Card title="Stock Split / Merge Notification" center>
+        <div className={styles.contents}>
+          <header className={styles.header}>
+            The following asset will be <strong>delisted</strong> due to a stock
+            split / merge.
+          </header>
+          <section className={styles.info}>{symbols}</section>
+          <ul className={styles.list}>
+            <li>
+              <p>
+                <strong>Delisted assets can be</strong> sent, withdrawn from
+                liquidity pools and used to close existing mint positions.
+              </p>
+            </li>
+            <li>
+              <p>
+                <strong>Delisted assets cannot be</strong> traded, minted or
+                provided to liquidity pools. LP staking rewards will immediately
+                stop.
+              </p>
+            </li>
+            <li>
+              <p>
+                You may burn your <strong>{symbols} (delisted)</strong> and
+                claim UST at the last price before stock split / merge event.
+              </p>
+            </li>
+          </ul>
+          <section className={styles.info}>
+            {tokens.map((token) => (
+              <p key={token}>
+                {[
+                  `1 ${getSymbol(token)} (delisted)`,
+                  `${format(find(priceKey, token))} ${UST}`,
+                ].join(" = ")}
+              </p>
+            ))}
+          </section>
+          <ul className={styles.list}>
+            <li>
+              <p>
+                You may trade, mint, provide liquidity for and stake LP tokens
+                for the new asset immediately after the stock split / merge.
+              </p>
+            </li>
+          </ul>
 
-        <section className={styles.info}>
-          {tokens.map(getSymbol).join(", ")}
-        </section>
+          <p className={styles.italic}>
+            Note that when UST to be returned after burning becomes
+            insufficient, you may receive other mAssets. Please close mint
+            positions with collateral other than UST before or immediately after
+            stock split / merge to avoid any losses.
+          </p>
 
-        <p>
-          Delisted assets can be sent, withdrawn from liquidity pools and used
-          to close existing mint positions.
-          <br />
-          Delisted assets cannot be traded, minted or provided to liquidity
-          pools. LP staking rewards will immediately stop.
-          <br />
-          You may burn your mKSU (delisted) and claim UST at the last price
-          before stock split / merge event.
-        </p>
+          <ExtLink
+            href="https://docs.mirror.finance/protocol/mirrored-assets-massets#deprecation-and-migration"
+            className={styles.link}
+          >
+            How does stock split/merge work on Mirror Protocol?
+          </ExtLink>
 
-        <section className={styles.info}>
-          {tokens.map((token) => (
-            <p key={token}>
-              {[
-                `1 ${getSymbol(token)} (delisted)`,
-                `${format(find(priceKey, token))} ${UST}`,
-              ].join(" = ")}
-            </p>
-          ))}
-        </section>
-
-        <p>
-          You may trade, mint, provide liquidity for and stake LP tokens for the
-          new asset immediately after the stock split / merge.
-          <br />
-          Note that when UST to be returned after burning becomes insufficient,
-          you may receive other mAssets. Please close mint positions with
-          collateral other than UST before or immediately after stock split /
-          merge to avoid any losses.
-        </p>
+          <Button onClick={modal.close} size="lg" block>
+            I understrand
+          </Button>
+        </div>
       </Card>
     </Modal>
   )
